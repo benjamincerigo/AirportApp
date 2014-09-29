522ce1c3a88ec90cad3213db6fcd44586e070089
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2014-09-29 10:06:25 +0200

    Working verison of the flights as tab add.

--- web/js/view1/search-flight.js
+++ web/js/view1/search-flight.js
@@ -16,7 +16,10 @@ angular.module('myApp.searchFlight', ['ngRoute'])
 	//Main Contorller For search FLight
 	
 	$scope.addAFlight = function(id){
-		console.log(id);
+		var flight = {'id':id};
+		$scope.flights.push(flight);
+		$scope.select(flight);
+		console.log($scope.flights);
 	}
 
 }])
@@ -39,8 +42,58 @@ angular.module('myApp.searchFlight', ['ngRoute'])
 	//flight view list directive
 	return{
 		restrict: 'E',
-		template: '<div></div>'
+		transclude: true,
+		templateUrl: 'js/view1/flight-list.html',
+		controller: ['$scope', function($scope){
+			var flights = $scope.flights = [];
+
+
+			$scope.select = function(flight) {
+
+		          angular.forEach(flights, function(flight) {
+		          	
+		            flight.selected = false;
+		          });
+		          flight.selected = true;
+		          
+		       };
+
+		     this.removeFlight = function(flight){
+		     	/*var position = $.inArray(flight.id, flights);
+		     	if(position) flights.slice(position, 1);*/
+
+		     	
+		     }
+
+			
+		}],
+
+		
+
+		
+		
+
 	}
 
+
+
+})
+
+.directive('flightView', function(){
+	return{
+		restrict: 'E',
+		require: "^flightList",
+		transclude: true,
+		templateUrl: 'js/view1/flight-view.html',
+		scope:{
+			flight: '=info',
+			
+		},
+		link: function(scope, element, attrs, flListCrtl){
+			
+			
+		}
+
+	}
 
 })
